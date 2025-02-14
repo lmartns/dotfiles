2674fb94d3d4f2492e61f3cfa255b71c1ff17783
lmartns <leonardomartins578@gmail.com>
2025-02-14 10:29:12 -0300

    adjust

--- lua/lmartns/plugins/bufferline.lua
+++ lua/lmartns/plugins/bufferline.lua
@@ -1,11 +1,14 @@
 return {
-  "akinsho/bufferline.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  version = "*",
-  opts = {
-    options = {
-      mode = "tabs",
-      separator_style = "slant",
-    },
-  },
-}
+	"akinsho/bufferline.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },  -- Dependência necessária
+	version = "*",
+	config = function()
+	  require("bufferline").setup({
+		options = {
+		  mode = "tabs",
+		  separator_style = "slant",
+		},
+	  })
+	end
+  }
+  
--- lua/lmartns/plugins/colorscheme.lua
+++ lua/lmartns/plugins/colorscheme.lua
@@ -1,7 +1,26 @@
 return {
-  "rebelot/kanagawa.nvim",
-  priority = 1000,
-  config = function()
-    vim.cmd("colorscheme kanagawa-wave")
-  end
-}
+	"rebelot/kanagawa.nvim",
+	lazy = false, -- Carregar imediatamente
+	priority = 1000, -- Definir prioridade alta
+	config = function()
+	  require("kanagawa").setup({
+		compile = false,             -- Ativar compilação para melhor desempenho (use `:KanagawaCompile` após mudanças)
+		undercurl = true,            -- Ativar undercurls
+		commentStyle = { italic = true },
+		keywordStyle = { italic = true },
+		statementStyle = { bold = true },
+		transparent = false,         -- Desativar fundo transparente
+		dimInactive = false,         -- Não escurecer janelas inativas
+		terminalColors = true,       -- Aplicar cores ao terminal
+		theme = "wave",              -- Padrão: "wave" (pode ser "dragon" ou "lotus")
+		background = {               -- Definir cores para os modos claro/escuro
+		  dark = "dragon",
+		  light = "lotus",
+		},
+	  })
+  
+	  -- Aplicar o esquema de cores
+	  vim.cmd("colorscheme kanagawa")
+	end
+  }
+  
